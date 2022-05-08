cd60989fe92820457ca18c14a133cc96e54df12f
Julieta Delgado <julidmeza@gmail.com>
2022-05-07 21:48:37 -0500

    Add CRUD endpoints for missionCommander

--- server.js
+++ server.js
@@ -115,3 +115,49 @@ app.delete('/students/:id', async (req, res) => {
   await prisma.student.delete({where: {id: id}});
   return res.json({message: "Student - Eliminado correctamente"});
 });
+
+app.get('/mission-commanders', async (req, res) => {
+  const allMissionCommanders =  await prisma.missioncommander.findMany({});
+  res.json(allMissionCommanders);
+});
+
+app.get('/mission-commanders/:id', async (req, res) => {
+  const id = req.params.id;
+  const missioncommander = await prisma.missioncommander.findUnique({where: {id: parseInt(id)}});
+  res.json(missioncommander);
+});
+
+app.post('/mission-commanders', async (req, res) => {
+  const missioncommander = {
+    name: req.body.name,
+    username: req.body.username,
+    mainStack: req.body.mainStack,
+    currentEnrollment: req.body.currentEnrollment,
+    hasAzureCertification: req.body.hasAzureCertification
+
+  };
+  const message = 'Mission Commander creado.';
+  await prisma.missioncommander.create({data: missioncommander});
+  return res.json({message});
+});
+
+app.put('/mission-commanders/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  await prisma.missioncommander.update({
+    where: {
+      id: id
+    },
+    data: {
+      mainStack: req.body.mainStack
+    }
+  })
+
+  return res.json({message: "Actualizado correctamente"});
+});
+
+app.delete('/mission-commanders/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  await prisma.missioncommander.delete({where: {id: id}});
+  return res.json({message: "Eliminado correctamente"});
+});
